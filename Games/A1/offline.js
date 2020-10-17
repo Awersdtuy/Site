4f7af52a0960e67e069a0f8cc9963629a790b575
Awersdtuy <Vladislav.Yachnovets_@hotmail.com>
2020-10-17 20:30:06 +0300

    Initial commit

---
+++ Games/A1/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1601044636,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/wall-sheet0.png",
+		"images/block-sheet0.png",
+		"images/pig-sheet0.png",
+		"images/block2-sheet0.png",
+		"images/block3-sheet0.png",
+		"images/wall2-sheet0.png",
+		"images/stoper-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
